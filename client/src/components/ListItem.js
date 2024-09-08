4e1b364816ce4b49ce9bed4109252be8ff406a3e
khk980703 <khk980703@hotmail.com>
2024-09-08 13:19:44 +1200

    third commit

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -3,14 +3,19 @@ import ProgressBar from './ProgressBar'
 
 const ListItem = ({task}) => {
     return (
-      <div className="list-item">
+      <li className="list-item">
+
         <div className="info-container">
             <TickIcon/>
             <p className="task-title">{task.title}</p>
             <ProgressBar/>
         </div>
-        <p>{task.title}</p>
-      </div>
+        
+        <div className='button-container'>
+            <button className='edit'>EDIT</button>
+            <button className='delete'>DELETE</button>
+        </div>
+      </li>
     )
   }
   
